eb6f44d126df39b93e1eabd1a982d5f6e6e87502
aortegaCampanillas <antonio.ortega@iescampanillas.com>
2022-10-19 21:05:07 +0200

    primer cambio en script 1

--- Assets/Scripts/Script1.cs
+++ Assets/Scripts/Script1.cs
@@ -7,7 +7,7 @@ public class Script1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Primer cambio en mi script
     }
 
     // Update is called once per frame
